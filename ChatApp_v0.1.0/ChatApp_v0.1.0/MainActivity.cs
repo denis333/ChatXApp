3b5e3ff9acd671ed9777cecff17c78cea1d11e19
denis333 <denis12345614@gmail.com>
2019-05-02 19:08:09 +0300

    commit 2

---
+++ ChatApp_v0.1.0/ChatApp_v0.1.0/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+
+namespace ChatApp_v0._1._0
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            SetupEvents();
+        }
+
+        private void SetupEvents()
+        {
+            // Events Subscription
+            Button btnEnter = FindViewById<Button>(Resource.Id.btnChatEnter);
+            EditText etUsername = FindViewById<EditText>(Resource.Id.etUsername);
+
+            btnEnter.Click += (sender, e) => SetContentView(Resource.Layout.activity_chatRoom);
+        }
+    }
+}
