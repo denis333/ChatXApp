86212b89b1506d687c497c20c3c932ea5c5380e9
denis333 <denis12345614@gmail.com>
2019-05-04 09:13:06 +0300

    XmppUtilities folder created

---
+++ ChatApp_v0.1.0/ChatApp_v0.1.0/XmppUtilities/RegisterGuest.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Matrix;
+using Matrix.Xmpp.Register;
+using Matrix.Xmpp.XData;
+
+namespace ChatApp_v0._1._0.XmppUtilities
+{
+    // TODO
+    public class RegisterGuest : IRegister
+    {
+        private XmppClient guestClient;
+        public RegisterGuest()
+        {
+            guestClient = new XmppClient
+            {
+                Username = "guest1",    // TODO: this name shall be dynamic
+                Password = "1111",
+                XmppDomain = "myDomain@example.com"
+            };
+        }
+
+        public bool RegisterNewAccount => true;
+
+        public async Task<Register> RegisterAsync(Register register)
+        {
+            return await Task<Register>.Run(() =>
+            {
+                register.RemoveAll<Data>();
+                register.Username = guestClient.Username;
+                register.Password = guestClient.Password;
+
+                return register;
+            });
+        }
+    }
+}
